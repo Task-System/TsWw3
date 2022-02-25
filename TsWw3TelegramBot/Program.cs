ffd25afd02ee492dc17337add196c4d43ac98caa
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-02-25 22:21:21 +0330

    Updates resourses

--- TsWw3TelegramBot/Program.cs
+++ TsWw3TelegramBot/Program.cs
@@ -2,41 +2,33 @@ using TelegramUpdater.Hosting;
 using TsWw3TelegramBot;
 using TsWw3TelegramBot.UpdateHandlers.Messages;
 
-var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(ServiceConfigurations);
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        var botConfigs = ctx.Configuration.GetSection("BotConfigs").Get<BotConfigs>();
 
-// Build the host.
-var host = builder.Build();
+        services.AddTelegramUpdater<PollingUpdateWriter>(
+            botConfigs.BotToken?? throw new Exception("BotToken cannot be null."),
+            default, // default options
+            (builder) => builder
+                .AddExceptionHandler<Exception>(
+                    (u, e) =>
+                    {
+                        u.Logger.LogWarning(exception: e, message: "Error while handlig ...");
+                        return Task.CompletedTask;
+                    }, inherit: true)
+
+                // Add handlers here.
+                .AddMessageHandler<StartHandler>(),
+
+            typeof(PreUpdateProcessor));
+
+        services.AddLocalization(options =>
+        {
+            options.ResourcesPath = "Resources";
+        });
+    })
+    .Build();
 
 // Run the host
 await host.RunAsync();
-
-/// <summary>
-/// A function to configure services for the host.
-/// </summary>
-static void ServiceConfigurations(HostBuilderContext ctx, IServiceCollection services)
-{
-    var botConfigs = ctx.Configuration.GetSection("BotConfigs").Get<BotConfigs>();
-
-    if (botConfigs.BotToken is null)
-        throw new Exception("BotToken cannot be null.");
-
-    services.AddTelegramUpdater<PollingUpdateWriter>( // Use your own manual update writer!
-        botConfigs.BotToken,
-        default, // default options
-        (builder) => builder
-            .AddExceptionHandler<Exception>(
-                (u, e) =>
-                {
-                    u.Logger.LogWarning(exception: e, message: "Error while handlig ...");
-                    return Task.CompletedTask;
-                }, inherit: true)
-
-            .AddMessageHandler<StartHandler>(),
-        typeof(PreUpdateProcessor));
-
-    services.AddLocalization(options =>
-    {
-        options.ResourcesPath = "Resources";
-    });
-}
